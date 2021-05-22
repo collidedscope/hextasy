50f7e1a801ce2db9baf20942b1418ce89900a30d
Collided Scope <collidedscope+github@protonmail.com>
2021-05-22 15:56:29 -0400

    Lay the groundwork for a command-line interface

--- hextasy.cr
+++ hextasy.cr
@@ -1,4 +1,21 @@
 require "hextasy"
+require "option_parser"
+
+OptionParser.parse do |parser|
+  parser.banner = <<-EOS
+  Hextasy is a Hexagony interpreter.
+  Usage: #{PROGRAM_NAME} [OPTIONS] FILE
+  EOS
+
+  parser.on("-h", "--help", "Show this help") {
+    abort parser
+  }
+
+  parser.invalid_option do |flag|
+    STDERR.puts "ERROR: #{flag} is not a valid option."
+    abort parser
+  end
+end
 
 source = if path = ARGV[0]?
            File.read path
