92181ae5c9f724dcd37bd4ccd1e32eeee3e5f377
Collided Scope <collidedscope+github@protonmail.com>
2021-05-22 03:05:46 -0400

    C̶o̶m̶p̶l̶i̶c̶a̶t̶e̶ Simplify `Heading` methods with a sweet macro

--- lib/hextasy/heading.cr
+++ lib/hextasy/heading.cr
@@ -9,85 +9,42 @@ struct Hextasy::Axpoint
   }
 
   enum Heading
-    {% for heading, vector in HEADINGS %}
+    {% for heading in HEADINGS.keys %}
       {{heading.capitalize.id}}
     {% end %}
 
-    def reverse
-      case self
-      when .northwest?; Southeast
-      when .northeast?; Southwest
-      when .east?     ; West
-      when .southeast?; Northwest
-      when .southwest?; Northeast
-      else              East
-      end
-    end
-
-    def slash
-      case self
-      when .northwest?; East
-      when .northeast?; Northeast
-      when .east?     ; Northwest
-      when .southeast?; West
-      when .southwest?; Southwest
-      else              Southeast
-      end
-    end
-
-    def backslash
-      case self
-      when .northwest?; Northwest
-      when .northeast?; West
-      when .east?     ; Southwest
-      when .southeast?; Southeast
-      when .southwest?; East
-      else              Northeast
-      end
-    end
-
     def lt(edge)
-      case self
-      when .northwest?; West
-      when .northeast?; Southwest
-      when .east?     ; edge > 0 ? Southeast : Northeast
-      when .southeast?; Northwest
-      when .southwest?; West
-      else              East
-      end
+      east? ? (edge > 0 ? Southeast : Northeast) : ltprime
     end
 
     def gt(edge)
-      case self
-      when .northwest?; Southeast
-      when .northeast?; East
-      when .east?     ; West
-      when .southeast?; East
-      when .southwest?; Northeast
-      else              edge > 0 ? Northwest : Southwest
-      end
-    end
-
-    def underscore
-      case self
-      when .northwest?; Southwest
-      when .northeast?; Southeast
-      when .east?     ; East
-      when .southeast?; Northeast
-      when .southwest?; Northwest
-      else              West
-      end
-    end
-
-    def pipe
-      case self
-      when .northwest?; Northeast
-      when .northeast?; Northwest
-      when .east?     ; West
-      when .southeast?; Southwest
-      when .southwest?; Southeast
-      else              East
-      end
+      west? ? (edge > 0 ? Northwest : Southwest) : gtprime
     end
   end
+
+  EXPAND = {NW: Northwest, NE: Northeast, E: East,
+            SE: Southeast, SW: Southwest, W: West}
+
+  macro def_mirror(name, mapping)
+    {% begin %}
+      enum Heading
+        def {{name}}
+          case self
+            {% for pair in mapping.split %}
+              in {{EXPAND[pair.split("->")[0]]}}
+                 {{EXPAND[pair.split("->")[1]]}}
+            {% end %}
+          end
+        end
+      end
+    {% end %}
+  end
+
+  def_mirror reverse,    "NW->SE  NE->SW  E->W   SE->NW  SW->NE  W->E"
+  def_mirror slash,      "NW->E   NE->NE  E->NW  SE->W   SW->SW  W->SE"
+  def_mirror backslash,  "NW->NW  NE->W   E->SW  SE->SE  SW->E   W->NE"
+  def_mirror underscore, "NW->SW  NE->SE  E->E   SE->NE  SW->NW  W->W"
+  def_mirror pipe,       "NW->NE  NE->NW  E->W   SE->SW  SW->SE  W->E"
+  def_mirror ltprime,    "NW->W   NE->SW  E->E   SE->NW  SW->W   W->E"
+  def_mirror gtprime,    "NW->SE  NE->E   E->W   SE->E   SW->NE  W->W"
 end
