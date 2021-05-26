6e36c2df98c9bc12be92ccf8db392b1a3f85d62d
Collided Scope <collidedscope+github@protonmail.com>
2021-05-26 00:26:28 -0400

    Add `-i`/`--input` flag for supplying input out of band

--- hextasy.cr
+++ hextasy.cr
@@ -1,14 +1,20 @@
 require "hextasy"
 require "option_parser"
 
+input = STDIN
 grid = nil
 pretty = nil
 
 OptionParser.parse do |parser|
   parser.banner = <<-EOS
   Hextasy is a Hexagony interpreter.
-  Usage: #{PROGRAM_NAME} [OPTIONS] FILE
+  Usage: #{PROGRAM_NAME} [OPTIONS] [FILE]
   EOS
+
+  parser.on("-i STR", "--input STR",
+    "Input for the read instructions (when stdin isn't suitable)") { |s|
+    input = IO::Memory.new s
+  }
 
   parser.on("-g N", "--grid N",
     "Display an empty hexagonal grid of radius N and exit") { |n|
@@ -39,5 +45,5 @@ h = Hextasy::Hexagony.new source
 if pretty
   puts h.lines
 else
-  h.interpret
+  h.interpret input
 end
